03fafa986510af349fc0cd5620500915c1ae777d
samirdewan1995 <samirdewan1995@gmail.com>
2022-03-27 23:02:53 +0100

    final draft

--- routes/clothesView.js
+++ routes/clothesView.js
@@ -4,6 +4,6 @@ const Controller = require("../controller/clothesController");
 
 router.get("/wash", Controller.getAllClothes);
 router.post("/newclothes", Controller.postClothes); //gives back an id, that id will be used for the userid.
-router.get("/item/:id", Controller.getClothesByID);
+router.get("/items/:id", Controller.getClothesByID);
 
 module.exports = router;
